f44cbc44bc81b416b1774d5bd669dcbab1079d56
ngoc-jewel <71751670+ngoc-jewel@users.noreply.github.com>
2025-12-20 14:00:26 +0700

    feat: thêm chức năng đăng nhập, đăng ký và đăng xuất

--- .github/cart/checkout.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html lang="vi">
-<head><meta charset="utf-8"><title>Thanh toán</title></head>
-<body>
-<h2>Thanh toán</h2>
-<p>Trang mô phỏng thanh toán. Bạn sẽ tích hợp sau (VNPAY/Momo/COD).</p>
-</body>
-</html>
